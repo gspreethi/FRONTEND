91244f64136df41522e488bc9540f20fa6f375aa
gspreethi <88823470+gspreethi@users.noreply.github.com>
2021-08-25 09:30:21 +0530

    Update UserPage.js

--- UserPage.js
+++ UserPage.js
@@ -18,23 +18,20 @@ export default function UserPage(props) {
     }
 
     return (
-        
-        <div>
-            <button className="btn btn-primary" onClick={()=>setButtonPopup(true)} style={{float:"right"}}>LogOut</button>
-                   <div>
-        {/* <nav className="nav-item "  style={{color:"white",fontSize:"35px"}}>
-         <Link className="nav-link" to="/TailorSearch" style={{color:"white"}}><AiOutlineSearch />Search for Tailor</Link>
-        </nav>
-        <PaymentRequest />
-        <nav className="nav-item "  style={{color:"white",fontSize:"35px"}}>
-         <Link className="nav-link" to="/ViewByOrder" style={{color:"white"}}>ViewByOrder</Link>
-        </nav> */}
-        </div>
-            
-            
-            
-        
-           
-        </div>
+        <div>   
+        <h2 style={{textAlign:'center',color:'black'}}>WELCOME!</h2>
+        <h3 style={{textAlign:'center',color:'skyblue'}}>Browse to select your gift</h3>
+        <button className="btn btn-primary" onClick={logout} style={{paddingLeft:"10px",paddingRight:"10px",marginTop:'10px',marginLeft:'1100px',marginRight:'50px'}}>LogOut</button>
+    <nav className="nav-item "  style={{marginTop:'20px',textAlign:'center'}}>
+     <Link className="nav-link" to="/BrowseCategory" style={{fontSize:"35px",color:"burlywood"}}> <b>Browse Categories</b></Link>
+    </nav>
+    {/* <nav className="nav-item "  style={{marginTop:'5px',textAlign:'center'}}>
+     <Link className="nav-link" to="/AddItem" style={{fontSize:"35px",color:"black"}} ><b>Add Item</b></Link>
+    </nav>
+    {/* <nav className="nav-item "  style={{marginTop:'5px',textAlign:'center'}}> 
+     <Link className="nav-link" to="/Delete" style={{fontSize:"35px",color:"black"}} ><b>Delete </b></Link>
+    </nav> */}
+    {/* <ViewOrders /> */}
+    </div>
     )
 }
